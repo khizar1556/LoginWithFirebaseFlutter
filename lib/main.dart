35e8aa4e025051b3cd43617351ce4384be0f01b9
muneebshahzad <muneebshahzad095@gmail.com>
2022-08-01 19:15:51 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:loginform/classes/googlesignin.dart';
+import 'package:loginform/classes/utils_snackbar.dart';
+import 'package:loginform/screens/signuppage.dart';
+import 'package:provider/provider.dart';
+
+import 'screens/loginpage.dart';
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (create) => GoogleSignInProvider(),
+      child:  MaterialApp(
+        scaffoldMessengerKey: Utils.messengerKey,
+        debugShowCheckedModeBanner: false,
+        home: LoginScreen(),
+      )
+  );
+
+
+}
+
+
+
+
+
